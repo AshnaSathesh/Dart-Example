744d79ad8cec7db2d93aa1476a999758eec71938
Raoof Naushad <raoof@accubits.com>
2020-03-18 01:50:08 +0530

    adding sample

---
+++ sample.dart
@@ -0,0 +1,13 @@
+class tech{
+
+  var person, time;
+  tech=(this.person,this.time);
+  var get person=> mentor || learner;
+  set person()=> time=mentor &&( time ?? 0);
+  var get stack=> ui ||webdevlp||go||python||flutter
+  set stack(var value)=> noofmentors=value && time  
+}
+
+void main(){
+  var tech=('mentor',2);
+   
